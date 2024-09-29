8fc68e96b978fa2a92777c253c2a3cea78a5236e
creatior <vartem2020@mail.ru>
2024-09-29 20:04:46 +0300

    Площадь треугольника
    Добавлена отрисовка внешних точек и подсчет абсолютной и относительной погрешностей

--- TriangleArea/MainWindow.xaml.cs
+++ TriangleArea/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using OxyPlot.Series;
 using OxyPlot.Axes;
 using System.Numerics;
 using System.Security.Cryptography;
+using System.Windows.Media.Animation;
 
 namespace TriangleArea
 {
@@ -27,6 +28,10 @@ namespace TriangleArea
     {
         private int n = 4; // номер варианта
         private PlotModel plotModel = new PlotModel { Title = "График функции f(x)" };
+        private ScatterSeries scatterSeriesInner;
+        private ScatterSeries scatterSeriesOuter;
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -119,23 +124,48 @@ namespace TriangleArea
 
         private void calculate_button_Click(object sender, RoutedEventArgs e)
         {
+
             if (N_textbox.Text != "")
             {
+                var seriesToRemoveInner = plotModel.Series.FirstOrDefault(s => s.Title == "Внутренние точки") as ScatterSeries;
+                var seriesToRemoveOuter = plotModel.Series.FirstOrDefault(s => s.Title == "Внешние точки") as ScatterSeries;
+
+                if (seriesToRemoveInner != null && seriesToRemoveOuter != null)
+                {
+                    plotModel.Series.Remove(seriesToRemoveInner);
+                    plotModel.Series.Remove(seriesToRemoveOuter);
+                }
                 int N = Int32.Parse(N_textbox.Text);
                 Tuple<double, List<(double, double)>, List<(double, double)>> answer = AreaCalculation(N);
                 answer_textbox.Text = $"Площадь треугольника: {Math.Round(answer.Item1, 4)}";
 
+                double absolute_error = Math.Abs(100.0 - answer.Item1);
+                absolute_error_textbox.Text = $"Абсолютная погрешность: {Math.Round(absolute_error, 2)}";
+
+                double relative_error = absolute_error / 100.0;
+                relative_error_textbox.Text = $"Относительная погрешность: {Math.Round(relative_error, 4)}";
+
                 List<(double, double)> random_points = answer.Item2;
                 List<(double, double)> inner_points = answer.Item3;
 
-                ScatterSeries scatterSeries = new ScatterSeries { MarkerType = MarkerType.Circle };
+                List<(double, double)> outer_points = random_points.Except(inner_points).ToList();
+
+                this.scatterSeriesInner = new ScatterSeries { Title = "Внутренние точки", MarkerType = MarkerType.Circle, MarkerFill = OxyColors.Orange, MarkerSize = 4 };
                 foreach (var point in inner_points)
                 {
-                    scatterSeries.Points.Add(new ScatterPoint(point.Item1, point.Item2));
+                    this.scatterSeriesInner.Points.Add(new ScatterPoint(point.Item1, point.Item2));
                 }
 
-                plotModel.Series.Add(scatterSeries); 
+                this.scatterSeriesOuter = new ScatterSeries { Title = "Внешние точки", MarkerType = MarkerType.Circle, MarkerFill = OxyColors.Red, MarkerSize = 4 };
+                foreach (var point in outer_points)
+                {
+                    this.scatterSeriesOuter.Points.Add(new ScatterPoint(point.Item1, point.Item2));
+                }
+
+                plotModel.Series.Add(scatterSeriesInner);
+                plotModel.Series.Add(scatterSeriesOuter);
                 PlotView.Model = plotModel;
+                PlotView.InvalidatePlot();
             }
         }
     }
